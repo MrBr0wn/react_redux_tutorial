5021a4e92bf29e95b20a92b5660de9a9ce6b9cbf
Mr. Brown <shamil52.93@gmail.com>
2018-01-21 00:17:03 +0500

    Babel 6 install, add presets for babel, edit webpack.config.js, created React component, install react-hot-loader, change package.json versions modules

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ module.exports = {
 	devtool: 'cheap-module-eval-source-map',
 	entry: [
 		'webpack-hot-middleware/client',
+		'babel-polyfill',
 		'./src/index'
 	],
 	output: {
@@ -13,8 +14,26 @@ module.exports = {
 		publicPath: '/static/'
 	},
 	plugins: [
-		// new webpack.optimize.OccurrenceOrderPlugin(),
+		/*
+		new webpack.optimize.OccurrenceOrderPlugin(),
 		new webpack.HotModuleReplacementPlugin(),
 		new webpack.NoEmitOnErrorsPlugin()
-	]
+		*/
+
+		new webpack.optimize.OccurenceOrderPlugin(),
+		new webpack.HotModuleReplacementPlugin(),
+		new webpack.NoErrorsPlugin()
+	],
+	module: {
+		loaders: [
+			{
+				loaders: ['react-hot', 'babel-loader'],
+				include: [
+					path.resolve(__dirname, 'src'),
+				],
+				test: /\.js$/,
+				plugins: ['transform-runtime'],
+			}
+		]
+	}
 }
